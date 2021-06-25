f44230abd23c6315af1c351b97a44a77088d39a4
ngothilinh20187179 <ngothilinh12915@gmail.com>
2021-06-25 14:30:05 +0700

    file explorer

---
+++ FILE_EXPLORER/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace FILE_EXPLORER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "This PC");
+            this.imageList1.Images.SetKeyName(1, "Disk");
+            this.imageList1.Images.SetKeyName(2, "Folder");
+            this.imageList1.Images.SetKeyName(3, "File");
+            this.imageList1.Images.SetKeyName(4, "Image");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 515);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 512);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(292, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // treeView1
+            // 
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(7, 6);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(334, 490);
+            this.treeView1.TabIndex = 5;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(358, 6);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(803, 526);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.StateImageList = this.imageList1;
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            this.listView1.ItemActivate += new System.EventHandler(this.listView1_ItemActivate);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 557);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Window Explorer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ FILE_EXPLORER/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FILE_EXPLORER
+{
+    public partial class Form1 : Form
+    {   
+        //Khai bao bien toan cuc cho thu muc hien tai
+        DirectoryInfo currentDir;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void initialize_treeView()
+        {
+            //Tao 1 root node, dat ten la This PC, add root node vao treeview
+            TreeNode thisPC = new TreeNode("This PC");
+            thisPC.Tag = "This PC";
+            // set image cho root node (This PC)
+            thisPC.ImageIndex = 0;
+            thisPC.SelectedImageIndex = 0;
+            treeView1.Nodes.Add(thisPC); 
+
+            // add cac disk(C,D,..) vao root node (This PC)
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                TreeNode driveNode = new TreeNode(drive.Name);
+                driveNode.Tag = drive.RootDirectory;
+                // set image cho disk C, D
+                driveNode.ImageIndex = 1;
+                driveNode.SelectedImageIndex = 1;
+                thisPC.Nodes.Add(driveNode);
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initialize_treeView();
+        }
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            textBox1.Text = treeView1.SelectedNode.FullPath; // sau khi chon node thi lay path hien thi vao textbox
+            TreeNode selectedNode = treeView1.SelectedNode;
+            try
+            {
+                if (selectedNode.Tag.GetType() == typeof(DirectoryInfo))
+                {
+                    // xoa cac node da co trong danh sach
+                    selectedNode.Nodes.Clear();
+                    // them cac node thu muc con
+                    DirectoryInfo dir = (DirectoryInfo)selectedNode.Tag;
+                    foreach (DirectoryInfo subDir in dir.GetDirectories())
+                    {
+                        TreeNode dirNode = new TreeNode(subDir.Name);
+                        dirNode.Tag = subDir;
+                        dirNode.ImageIndex = 2;
+                        dirNode.SelectedImageIndex = 3;
+                        selectedNode.Nodes.Add(dirNode);
+                    }
+                    // hien thi tren listview
+                    currentDir = dir;
+                    OpenDirectory();
+                }
+                selectedNode.Expand();
+            }
+            catch
+            {
+                MessageBox.Show("Can't open!");
+                return;
+            }
+        }
+        private void OpenDirectory()
+        {
+            listView1.Clear();
+            // them cac cot Name, Type, Size, Data Modified vao listView1
+            listView1.Columns.Add("Name", 200, HorizontalAlignment.Left);
+            listView1.Columns.Add("Size", 80, HorizontalAlignment.Center);
+            listView1.Columns.Add("Type", 80, HorizontalAlignment.Center);
+            listView1.Columns.Add("Date Modified", 230, HorizontalAlignment.Center);
+
+            // hien thi theo dang chi tiet
+            listView1.View = View.Details;
+
+            foreach (DirectoryInfo subDir in currentDir.GetDirectories())
+            {
+                ListViewItem lv1 = listView1.Items.Add(subDir.Name);
+                lv1.Tag = subDir;
+                lv1.ImageIndex = 2;
+                lv1.SubItems.Add("");
+                lv1.SubItems.Add("Folder");
+                lv1.SubItems.Add(subDir.LastWriteTime.ToString());
+            }
+            foreach (FileInfo file in currentDir.GetFiles())
+            {
+                ListViewItem lv1 = listView1.Items.Add(file.Name);
+                lv1.Tag = file;
+                lv1.ImageIndex = 3;
+                lv1.SubItems.Add(file.Length.ToString());
+                lv1.SubItems.Add("File");
+                lv1.SubItems.Add(file.LastWriteTime.ToString());
+            }
+        }
+        private void listView1_ItemActivate(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.SelectedItems[0].Tag.GetType() == typeof(DirectoryInfo))
+                {
+                    currentDir = (DirectoryInfo)listView1.SelectedItems[0].Tag;
+                    OpenDirectory();
+                }
+                else
+                {
+                    FileInfo file = (FileInfo)listView1.SelectedItems[0].Tag;
+                    System.Diagnostics.Process.Start(file.FullName);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Can't open!");
+                return;
+            }
+        }
+    }
+}
